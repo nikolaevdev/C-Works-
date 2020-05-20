ada860dcbe9389786e5a9b6d87bf46e23b279747
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-20 11:26:58 +0300

    Lib4 1 exp

--- 20.05.2020/Lib4_1/Lib4_1/Form1.Designer.cs
+++ 20.05.2020/Lib4_1/Lib4_1/Form1.Designer.cs
@@ -181,7 +181,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Lib4 2 exp";
             this.ResumeLayout(false);
             this.PerformLayout();
 
