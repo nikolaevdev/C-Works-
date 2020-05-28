60cbf5df22717833ec555860bda55939d708f808
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-28 16:37:48 +0300

    5_ex
    5.1 and 5,2

---
+++ 28.05.2020/5_1/5_1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Button btn = new Button(); //Новая кнопка
+                this.Controls.Add(btn); //Добавление кнопки в форму
+                btn.Location = new Point(e.X, e.Y); //координата e.X - x координата, координата e.Y - y координата
+                btn.Text = string.Format("{0}, {1}", e.X, e.Y); //Перевод в строку
+            }
+        }
+    }
+}
---
+++ 28.05.2020/9_2/9_2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void click(object sender, MouseEventArgs e)
+        {
+            Control element;
+
+            if (e.Location.X < (Size.Width / 2))
+            {
+                element = new Button() { 
+                    Text = "Кнопка" 
+                };
+                element.Size = new Size(120, 40);
+            }
+            else
+            {
+                element = new TextBox() {
+                    Text = "Поле ввода" 
+                };
+                element.Size = new Size(123, 20);
+            }
+
+            element.Location = e.Location;
+            this.Controls.Add(element);
+        }
+    }
+}
